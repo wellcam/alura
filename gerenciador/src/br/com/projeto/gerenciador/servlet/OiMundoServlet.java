2ef4355693f79fe0e3551406dbe64cb764e17d7a
Wellington Camilo <wellington.camilo@discover.com.br>
2019-08-27 16:53:08 -0300

    Novas atualizacoes

---
+++ gerenciador/src/br/com/projeto/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,27 @@
+package br.com.projeto.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/oi"})
+public class OiMundoServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Acessando servlet OiMundo");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
